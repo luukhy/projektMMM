78596e0f3dbb4d29e0e62033b2a663a0eaa37e2a
luukhy <hpiotrek080@gmail.com>
2025-05-30 19:10:57 +0200

    matplot test code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include<matplot/matplot.h>
 
-int main()
-{
-    std::cout << "hello world!";
+
+ int main()
+ {
+    using namespace matplot;
+    auto x = linspace(0, 2 * pi, 100);
+    auto y = transform(x, [](double x) { return sin(x); });
+    plot(x, y);
+    show();
     return 0;
-}
+ }
